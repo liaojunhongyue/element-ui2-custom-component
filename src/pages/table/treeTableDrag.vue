0293b89f0d3089c275b08d6158e0a9d5af8061ea
qhy <2421155310@qq.com>
2025-06-17 14:00:08 +0800

    add tree table mock data

--- src/pages/table/treeTableDrag.vue
+++ src/pages/table/treeTableDrag.vue
@@ -8,15 +8,18 @@
 			default-expand-all
 			:tree-props="{children: 'children', hasChildren: 'hasChildren'}">
 			<el-table-column
+				prop="id"
+				label="ID"
+				width="180">
+			</el-table-column>
+			<el-table-column
 				prop="date"
 				label="日期"
-				sortable
 				width="180">
 			</el-table-column>
 			<el-table-column
 				prop="name"
 				label="姓名"
-				sortable
 				width="180">
 			</el-table-column>
 			<el-table-column
@@ -31,36 +34,69 @@
     data() {
       return {
         tableData: [{
-          id: 1,
+          id: '1',
           date: '2016-05-02',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
+          address: '上海市普陀区金沙江路 1518 弄',
+					children: [{
+						id: '1-1',
+						date: '2016-05-01',
+						name: '王小虎',
+						address: '上海市普陀区金沙江路 1519 弄',
+						children: [{
+							id: '1-1-1',
+							date: '2016-05-01',
+							name: '王小虎',
+							address: '上海市普陀区金沙江路 1519 弄'
+						}, {
+							id: '1-1-2',
+							date: '2016-05-01',
+							name: '王小虎',
+							address: '上海市普陀区金沙江路 1519 弄'
+						}]
+					}, {
+						id: '1-2',
+						date: '2016-05-01',
+						name: '王小虎',
+						address: '上海市普陀区金沙江路 1519 弄'
+				}]
         }, {
-          id: 2,
+          id: '2',
           date: '2016-05-04',
           name: '王小虎',
           address: '上海市普陀区金沙江路 1517 弄'
         }, {
-          id: 3,
+          id: '3',
           date: '2016-05-01',
           name: '王小虎',
           address: '上海市普陀区金沙江路 1519 弄',
           children: [{
-              id: 31,
-              date: '2016-05-01',
-              name: '王小虎',
-              address: '上海市普陀区金沙江路 1519 弄'
-            }, {
-              id: 32,
-              date: '2016-05-01',
-              name: '王小虎',
-              address: '上海市普陀区金沙江路 1519 弄'
+						id: '3-1',
+						date: '2016-05-01',
+						name: '王小虎',
+						address: '上海市普陀区金沙江路 1519 弄'
+					}, {
+						id: '3-2',
+						date: '2016-05-01',
+						name: '王小虎',
+						address: '上海市普陀区金沙江路 1519 弄'
           }]
         }, {
-          id: 4,
+          id: '4',
           date: '2016-05-03',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
+          address: '上海市普陀区金沙江路 1516 弄',
+					children: [{
+						id: '4-1',
+						date: '2016-05-01',
+						name: '王小虎',
+						address: '上海市普陀区金沙江路 1519 弄'
+					}, {
+						id: '4-2',
+						date: '2016-05-01',
+						name: '王小虎',
+						address: '上海市普陀区金沙江路 1519 弄'
+          }]
         }],
       }
     },
